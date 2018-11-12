f95cb818a91eb375599f40a524ceece0132abff9
xiejunping <xiejunping@caohua.com>
2018-11-12 15:44:36 +0800

    router add

---
+++ app/model/auth.model.js
@@ -0,0 +1,11 @@
+const logger = require('../controllers/logger')
+
+/**
+ * 用户相关的数据库操作
+ * @param DB
+ */
+const auth = DB => {
+  return {}
+}
+
+module.exports = auth
---
+++ app/routes/auth.js
@@ -0,0 +1,25 @@
+const router = require('koa-router')();
+const DateFmt = require('../utils/date');
+const logger = require('../controllers/logger');
+const c = require('../controllers/decorator');
+
+router.prefix('/auth');
+
+router.get('/', async (ctx, next) => {
+  ctx.type = 'html';
+  await ctx.render('index', {
+    title: 'John'
+  });
+});
+
+router.get('/menu', c.oAuth, async (ctx, next) => {
+  const { id } = ctx.session.user;
+
+
+});
+
+router.get('/:id', c.oAuth, async (ctx, next) => {
+  const { id } = ctx.session.user;
+
+
+});
